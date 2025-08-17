c3ac42608152fa7ffdf740ae86a76233451ffc96
jurgengjoncari <j.gjoncari@albansi.com>
2025-08-17 02:55:04 +0200

    show messages in homepage

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,29 @@
-import { Component, signal } from '@angular/core';
+import {Component, inject} from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {HttpClient} from '@angular/common/http';
+import {environment} from '../environments/environment';
+import {DatePipe} from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [RouterOutlet, DatePipe],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('mini-message-board-fe');
+  protected readonly baseUrl = environment.apiUrl;
+  messages: any[] = [];
+  private http = inject(HttpClient);
+
+  constructor() {
+    this.getMessages();
+  }
+
+  getMessages() {
+    this.http.get<any[]>(`${this.baseUrl}/`).subscribe({
+      next: (data) => this.messages = data,
+      error: (err) => console.error(err)
+    });
+  }
 }
