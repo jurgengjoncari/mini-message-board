80c4f957576a5f72abb3959f969caaea632c9089
jurgengjoncari <j.gjoncari@albansi.com>
2025-08-17 09:16:06 +0200

    add input to send messages

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,13 +1,13 @@
 import {Component, inject} from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 import {HttpClient} from '@angular/common/http';
 import {environment} from '../environments/environment';
 import {DatePipe} from '@angular/common';
+import {FormsModule} from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, DatePipe],
+  imports: [DatePipe, FormsModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
@@ -15,6 +15,7 @@ export class App {
   protected readonly baseUrl = environment.apiUrl;
   messages: any[] = [];
   private http = inject(HttpClient);
+  newMessage: string = '';
 
   constructor() {
     this.getMessages();
@@ -23,6 +24,16 @@ export class App {
   getMessages() {
     this.http.get<any[]>(`${this.baseUrl}/`).subscribe({
       next: (data) => this.messages = data,
+      error: (err) => console.error(err)
+    });
+  }
+
+  sendMessage() {
+    this.http.post(`${this.baseUrl}/`, {content: this.newMessage}).subscribe({
+      next: (data) => {
+        this.messages.push(data);
+        this.newMessage = '';
+      },
       error: (err) => console.error(err)
     });
   }
